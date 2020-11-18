8df921320adfefdc94f5e1584acc7d01f662fb86
albertosfernandes <alberto.souzafernandes@gmail.com>
2020-11-18 11:18:08 -0300

    Adicionar arquivos de projeto.

---
+++ ServicesCeltaWare.UtilitariosInfra.Telegram/UtilTelegram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ServicesCeltaWare.UtilitariosInfra
+{
+    public class UtilTelegram
+    {
+        private string token;
+        public UtilTelegram(string _token)
+        {
+            this.token = _token;
+        }
+
+        public void SendMessage(string message, string destinationId)
+        {
+            try
+            {
+                Send(message, destinationId);
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+        }
+
+        private void Send(string text, string destID)
+        {
+            try
+            {
+                var bot = new Telegram.Bot.TelegramBotClient(token);
+                bot.SendTextMessageAsync(destID, text);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
